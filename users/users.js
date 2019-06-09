01b19b5d2eee554b33175226950937e051ccd7bd
nickbrinkmann <nick.brinkmann4@gmail.com>
2019-06-09 23:57:29 +0100

    Getting close

--- users/users.js
+++ users/users.js
@@ -142,121 +142,15 @@ router.post("/login", (req, res) => {
     );
 });
 
-// //Allows a user to update their information
-// router.post("/update", (req, res) => {
-//     const user = req.body;
-//     fs.readFile("./data/userdata.json", function (err, data) {
-//         if (err) {
-//             throw err;
-//         } else {
-//             if (data.length > 0) {
-//                 var parseData = JSON.parse(data);
-//                 parseData.users = parseData.users.filter(existingUser => {
-//                     return existingUser.email !== user.email;
-//                 });
+//Returns all users
+router.get("/", (req, res) => {
+    connection.query("SELECT * FROM user", (err, result) => {
+        if (err) {
+            return res.status(500).json({ message: "Failed to select"});
+        }
 
-//                 const updateUser = {
-//                     name: user.firstname,
-//                     lastname: user.lastname,
-//                     email: user.email,
-//                     password: user.password,
-//                     cellPhone: user.cellPhone,
-//                     role: user.role
-//                 };
-
-//                 parseData.users.push(updateUser);
-//                 fs.writeFile("./data/userdata.json", JSON.stringify(parseData), function (err) {
-//                     if (err) {
-//                         throw err;
-//                     }
-//                     res.json(updateUser);
-//                     console.log("User updated successfully");
-//                 });
-//             } else {
-//                 throw new Error("No users exist");
-//             }
-//         }
-//     });
-// });
-
-
-
-// ------------TRYING TO GET THE POSTMAN FUNCTIONS TO WORK WITH MYSQL-------------------
-
-// //Registers a new user
-// router.post("/register", (req, res) => {
-//     const user = req.body;
-
-//     //The code that follows checks if all necessary information was included in the http request.
-//     const bodyFirstname = user.firstname;
-//     const bodyLastname = user.lastname;
-//     const bodyEmail = user.email;
-//     const bodyPassword = user.password;
-
-//     var errors = [];
-//     if (!bodyFirstname) {
-//         errors.push({ message: "Invalid firstname" });
-//         // return res.status(400).json({message: "Invalid firstname"});
-//     }
-
-//     if (!bodyLastname) {
-//         errors.push({ message: "Invalid lastname" });
-//         // return res.status(400).json({message: "Invalid lastname"});
-//     }
-
-//     if (!bodyEmail) {
-//         errors.push({ message: "Invalid email" });
-//         // return res.status(400).json({message: "Invalid email"});
-//     }
-
-//     if (!bodyPassword) {
-//         errors.push({ message: "Invalid password" });
-//         // return res.status(400).json({message: "Invalid password"});
-//     }
-
-//     if (errors.length > 0) {
-//         return res.status(400).json({ errorMessages: errors });
-//     }
-
-//     //Checks user against existing user database, and if no errors, adds user to the database.
-//     fs.readFile("./data/userdata.json", function (err, data) {
-//         if (err) {
-//             throw err;
-//         } else {
-//             let newID = 0;
-
-//             //Checks email against database.
-//             if (data.length > 0) {
-//                 var parseData = JSON.parse(data);
-//                 parseData.users.forEach(existingUser => {
-//                     if (existingUser.email == user.email) {
-//                         return res.json({ message: "Email exists already!" });
-//                     } else {
-//                         newID++;
-//                     }
-//                 });
-//             }
-//             //Initializes user to database
-//             const newUser = {
-//                 id: (newID + 1).toString(),
-//                 firstname: user.firstname,
-//                 lastname: user.lastname,
-//                 email: user.email,
-//                 password: user.password,
-//                 cellphone: user.cellPhone,
-//                 role: user.role
-//             };
-//             parseData.users.push(newUser);
-//             res.json(newUser);
-//             fs.writeFile("./data/userdata.json", JSON.stringify(parseData), function (err) {
-//                 if (err) {
-//                     throw err;
-//                 }
-//                 console.log("Registration successful");
-//                 return parseData.users;
-//             });
-//         }
-//     });
-// });
+        return res.json(result);
+    });
+});
 
 module.exports = router;
